d47977469734ddf012666057b0b2617649804190
cppmonger <keskinemirhan@gmail.com>
2024-03-18 01:28:12 +0300

    Add config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AccountModule } from './account/account.module';
+import { Account } from './account/entities/account.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { envSchema } from './env.validation';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      type: configService.get<any>("DB_TYPE"),
+      database: configService.get<string>("DB_NAME"),
+      username: configService.get("DB_USERNAME"),
+      password: configService.get("DB_PASSWORD"),
+      port: configService.get("DB_PORT"),
+      synchronize: configService.get("DB_SYNCHRONIZE"),
+      host: configService.get("DB_HOST"),
+      entities: [Account],
+    })
+  }),
+    AccountModule,
+  ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: envSchema,
+    validationOptions: {
+      allowUnknown: true,
+      abortEarly: true
+    }
+  }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ backend/src/env.validation.ts
@@ -0,0 +1,19 @@
+import * as Joi from "joi";
+
+export const envSchema = Joi.object({
+  DB_TYPE: Joi.string().default("sqlite"),
+  DB_HOST: Joi.string(),
+  DB_NAME: Joi.string().default("db.sqlite"),
+  DB_PORT: Joi.number().port(),
+  DB_USERNAME: Joi.string(),
+  DB_PASSWORD: Joi.string(),
+  DB_SYNCHRONIZE: Joi.boolean().default(true),
+  SMTP_HOST: Joi.string().required(),
+  SMTP_PORT: Joi.number().port().required(),
+  SMTP_TLS: Joi.boolean().default(true),
+  SMTP_USERNAME: Joi.string().required(),
+  SMTP_PASSWORD: Joi.string().required(),
+  APP_HOST: Joi.string().default("localhost"),
+  APP_PORT: Joi.number().port().default(3000),
+  JWT_SECRET: Joi.string().default("secret"),
+});
