50a72281c4b97dd76ba3ba916c04301c689d1693
cppmonger <keskinemirhan@gmail.com>
2024-03-22 18:15:28 +0300

    Add validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(new ValidationPipe());
   const configService = app.get(ConfigService);
   const hostname = configService.get<string>("APP_HOSTNAME");
   const port = configService.get<number>("APP_PORT");
+
   await app.listen(port, hostname);
 }
 bootstrap();
