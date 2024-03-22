2c06e6db9f2603b7633c92e531e8437f7d18ac39
cppmonger <keskinemirhan@gmail.com>
2024-03-22 18:13:56 +0300

    Add query service model

---
+++ backend/src/model/query-service.ts
@@ -0,0 +1,38 @@
+import { NotFoundException } from "@nestjs/common";
+import { ExceptionCode } from "src/exception/exception";
+import { FindOptionsWhere, Repository } from "typeorm";
+
+export class QueryService<T> {
+  repo: Repository<T>;
+  notFoundCode: ExceptionCode;
+
+  constructor(repo: Repository<T>, notFoundCode: ExceptionCode) {
+    this.repo = repo;
+    this.notFoundCode = notFoundCode;
+  }
+
+  async getOne(where: FindOptionsWhere<T>) {
+    const item = await this.repo.findOne(where);
+    if (!item) throw new NotFoundException(this.notFoundCode);
+    return item;
+  }
+
+  async getAll(page: number, quantity: number, where?: FindOptionsWhere<T>) {
+    const [result, total] = await this.repo.findAndCount({ where, take: quantity, skip: (page - 1) * quantity })
+    const lastPage = Math.ceil(total / quantity);
+    const nextPage = page + 1 > lastPage ? null : page + 1;
+    const prevPage = page - 1 < 1 ? null : page - 1;
+
+    return {
+      result,
+      total,
+      currentPage: page,
+      nextPage,
+      prevPage,
+      lastPage,
+    }
+  }
+
+
+}
+
