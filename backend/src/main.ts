fc4df1e0c2daf35f6f64ce921ee9436993a48ff9
cppmonger <keskinemirhan@gmail.com>
2024-03-25 21:13:32 +0300

    Add whitelist option to global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
 
   const configService = app.get(ConfigService);
 
