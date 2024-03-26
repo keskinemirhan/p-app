70954beca144d5af1af92640e8b7a66639f4d027
cppmonger <keskinemirhan@gmail.com>
2024-03-26 22:22:26 +0300

    Remove contact info entity

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Capability } from "./entities/capability.entity";
-import { ContactInfo } from "./entities/contact-info.entity";
 import { Education } from "./entities/education.entity";
 import { Experience } from "./entities/experience.entity";
 import { Profile } from "./entities/profile.entity";
@@ -10,7 +9,7 @@ import { Reference } from "./entities/reference.entity";
 import { Review } from "./entities/review.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Capability, ContactInfo, Education, Experience, Profile, Project, Reference, Review])]
+  imports: [TypeOrmModule.forFeature([Capability, Education, Experience, Profile, Project, Reference, Review])],
 
 })
 export class ProfileModule { }
