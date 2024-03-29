d83235c16fef03b5ade59564b164b772cda8d478
cppmonger <keskinemirhan@gmail.com>
2024-03-29 16:52:08 +0300

    Update QueryService

--- backend/src/model/query-service.ts
+++ backend/src/model/query-service.ts
@@ -1,6 +1,6 @@
 import { NotFoundException } from "@nestjs/common";
 import { ExceptionCode } from "src/exception/exception";
-import { FindOptionsWhere, Repository } from "typeorm";
+import { FindManyOptions, FindOptionsRelations, FindOptionsWhere, Repository } from "typeorm";
 
 export class QueryService<T> {
   repo: Repository<T>;
@@ -11,14 +11,14 @@ export class QueryService<T> {
     this.notFoundCode = notFoundCode;
   }
 
-  async getOne(where: FindOptionsWhere<T>) {
-    const item = await this.repo.findOne({ where });
+  async getOne(where: FindOptionsWhere<T>, relations?: FindOptionsRelations<T>) {
+    const item = await this.repo.findOne({ where, relations });
     if (!item) throw new NotFoundException(this.notFoundCode);
     return item;
   }
 
-  async getAll(page: number, quantity: number, where?: FindOptionsWhere<T>) {
-    const [result, total] = await this.repo.findAndCount({ where, take: quantity, skip: (page - 1) * quantity })
+  async getAll(page: number, quantity: number, options?: FindManyOptions<T>) {
+    const [result, total] = await this.repo.findAndCount({ ...options, take: quantity, skip: (page - 1) * quantity })
     const lastPage = Math.ceil(total / quantity);
     const nextPage = page + 1 > lastPage ? null : page + 1;
     const prevPage = page - 1 < 1 ? null : page - 1;
