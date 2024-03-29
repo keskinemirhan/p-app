ee51069cf383840e4b9ec8cbb400bcc064ab9e58
cppmonger <keskinemirhan@gmail.com>
2024-03-29 16:52:54 +0300

    Add partial query result dto

---
+++ backend/src/model/query-result.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class QueryResultPartialDto {
+  @ApiProperty()
+  total: number;
+
+  @ApiProperty()
+  currentPage: number;
+
+  @ApiProperty()
+  nextPage: number;
+
+  @ApiProperty()
+  prevPage: number;
+
+  @ApiProperty()
+  lastPage: number;
+
+}
