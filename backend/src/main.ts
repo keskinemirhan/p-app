5d01153ec4fece8cf058519dbadba589e3b2b474
cppmonger <keskinemirhan@gmail.com>
2024-03-31 21:24:28 +0300

    Change site paths

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,30 +9,32 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, 'mvc', 'public'));
-  app.setBaseViewsDir(join(__dirname, 'mvc', 'views'));
-  app.setViewEngine("hbs");
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   const configService = app.get(ConfigService);
 
-  const appName = configService.get<string>("APP_NAME");
-  const appDesc = configService.get<string>("APP_DESCRIPTION");
+  const appName = configService.get<string>('APP_NAME');
+  const appDesc = configService.get<string>('APP_DESCRIPTION');
 
   const config = new DocumentBuilder()
     .setTitle(appName)
     .setDescription(appDesc)
-    .setVersion("1.0")
+    .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const hostname = configService.get<string>("APP_HOSTNAME");
-  const port = configService.get<number>("APP_PORT");
+  const hostname = configService.get<string>('APP_HOSTNAME');
+  const port = configService.get<number>('APP_PORT');
 
   await app.listen(port, hostname);
 }
