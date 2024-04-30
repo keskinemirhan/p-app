c943b4bb3a7a34b2ce312fe772306196e044288e
cppmonger <keskinemirhan@gmail.com>
2024-04-30 21:26:13 +0300

    Add apiProperty decorator to attr ids

--- backend/src/profile/entities/base-attr.entity.ts
+++ backend/src/profile/entities/base-attr.entity.ts
@@ -1,8 +1,10 @@
 import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Profile } from "./profile.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class BaseAttribute {
+  @ApiProperty()
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
