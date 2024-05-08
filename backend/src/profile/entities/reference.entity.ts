f7eea09b81bebf92ba81fa447b8cd26b3cf028fb
cppmonger <keskinemirhan@gmail.com>
2024-05-09 02:31:07 +0300

    Remove columns except referenceAccount

--- backend/src/profile/entities/reference.entity.ts
+++ backend/src/profile/entities/reference.entity.ts
@@ -1,21 +1,11 @@
-import { Account } from "src/account/entities/account.entity";
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne } from "typeorm";
 import { Profile } from "./profile.entity";
 import { BaseAttribute } from "./base-attr.entity";
 
 @Entity()
 export class Reference extends BaseAttribute {
-  @Column({ nullable: true })
-  name: string;
-
-  @Column({ nullable: true })
-  email: string;
-
-  @Column({ nullable: true })
-  website: string;
-
-  @ManyToOne(() => Account)
+  @ManyToOne(() => Profile, { onDelete: "CASCADE" })
   @JoinColumn()
-  referenceAccount: Account;
+  referenceAccount: Profile;
 }
 
