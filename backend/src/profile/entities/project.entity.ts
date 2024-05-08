8d42b12cb3ed3579701a6c15f0ae713bf53d7958
cppmonger <keskinemirhan@gmail.com>
2024-05-09 02:51:50 +0300

    Add ApiProperty decorators

--- backend/src/profile/entities/project.entity.ts
+++ backend/src/profile/entities/project.entity.ts
@@ -1,21 +1,23 @@
 import { Column, Entity } from "typeorm";
 import { BaseAttribute } from "./base-attr.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class Project extends BaseAttribute {
-
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty()
   @Column({ nullable: true })
   repoLink: string;
 
+  @ApiProperty()
   @Column()
   role: string;
 
+  @ApiProperty()
   @Column()
   isParticipatingNow: boolean;
-
-
 }
 
