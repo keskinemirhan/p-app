9964c2124736ccd5e34afc6378e585680acbd220
cppmonger <keskinemirhan@gmail.com>
2024-05-12 21:23:46 +0300

    Separate dataSourceOptions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,36 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountModule } from './account/account.module';
-import { Account } from './account/entities/account.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env.validation';
 import { MailerModule } from './mailer/mailer.module';
 import { AuthModule } from './auth/auth.module';
-import { EmailVerification } from './auth/entities/email-verification.entity';
 import { ProfileModule } from './profile/profile.module';
-import { Capability } from './profile/entities/capability.entity';
-import { Education } from './profile/entities/education.entity';
-import { Experience } from './profile/entities/experience.entity';
-import { Profile } from './profile/entities/profile.entity';
-import { Project } from './profile/entities/project.entity';
-import { Reference } from './profile/entities/reference.entity';
-import { Review } from './profile/entities/review.entity';
+import { dataSourceOption } from './db/ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-      type: configService.get<any>("DB_TYPE"),
-      database: configService.get<string>("DB_NAME"),
-      username: configService.get("DB_USERNAME"),
-      password: configService.get("DB_PASSWORD"),
-      port: configService.get("DB_PORT"),
-      synchronize: configService.get("DB_SYNCHRONIZE"),
-      host: configService.get("DB_HOST"),
-      entities: [Account, EmailVerification, Capability, Education, Experience, Profile, Project, Reference, Review],
-    })
-  }),
+  imports: [TypeOrmModule.forRoot(dataSourceOption),
     AuthModule,
     AccountModule,
     MailerModule,
---
+++ backend/src/db/ormconfig.ts
@@ -0,0 +1,29 @@
+import { required } from "joi";
+import { join } from "path";
+import { Account } from "src/account/entities/account.entity";
+import { EmailVerification } from "src/auth/entities/email-verification.entity";
+import { Capability } from "src/profile/entities/capability.entity";
+import { Education } from "src/profile/entities/education.entity";
+import { Experience } from "src/profile/entities/experience.entity";
+import { Profile } from "src/profile/entities/profile.entity";
+import { Project } from "src/profile/entities/project.entity";
+import { Reference } from "src/profile/entities/reference.entity";
+import { Review } from "src/profile/entities/review.entity";
+import { DataSource, DataSourceOptions } from "typeorm";
+require("dotenv").config();
+
+export const dataSourceOption: DataSourceOptions =
+{
+  type: "postgres",
+  database: process.env["DB_NAME"],
+  username: process.env["DB_USERNAME"],
+  password: process.env["DB_PASSWORD"],
+  port: Number(process.env["DB_PORT"]),
+  synchronize: process.env["DB_SYNCHRONIZE"] === "true" ? true : false,
+  host: process.env["DB_HOST"],
+  entities: [Account, EmailVerification, Capability, Education, Experience, Profile, Project, Reference, Review],
+  migrations: ["./src/db/migrations"],
+  migrationsTableName: "migrations",
+}
+
+export const dataSource = new DataSource(dataSourceOption);
